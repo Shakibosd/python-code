7abe27fef05098f71c64511f446987ffe81379af
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-04-12 05:36:21 +0600

    finish

---
+++ program 2.py
@@ -0,0 +1,11 @@
+'''
+Python Bangla Tutorials 4 : Backslash characters and comments
+'''
+# Escape sequence
+
+print("Shakib\n01401997130")
+
+print("Shakib\t01401997130")
+
+print("\"Shakib\"")
+
---
+++ program 3.py
@@ -0,0 +1,11 @@
+# Python Tutorials 5 : Variables and Data Types
+# Variables
+name = "Nokib"
+age = 20
+gpa = 3.10
+
+print("Our New Student Name Is "+name)
+print(name+" lives in Sylhet")
+print("She Is currectly ",age," years old")
+print("at the age of ",age," she hes started learn python")
+print(name+" has scored ",gpa," in her secondary exam")
