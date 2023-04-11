d033f603b640f6ee87b9fdfb10beb3d2c8dd767d
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-04-12 03:17:14 +0600

    first commit

---
+++ program 1.py
@@ -0,0 +1,13 @@
+#Python Tutorials 3 : First Python Program
+
+#Number Print
+print(10)
+
+#Flote Number print
+print(10.7)
+
+#My Name Print
+print("Shakib")
+
+#MY Phone Number Print 
+print("01401997030")
