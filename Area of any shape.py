05e9399e9d4a1c16c73e80c6424ee60cb1fc4e22
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-04-12 19:56:03 +0600

    finish

---
+++ Area of any shape.py
@@ -0,0 +1,21 @@
+# Python Bangla Tutorials 9 : Area of any shape
+'''
+Triangle
+Atra = 0.5* b* h
+b = base
+h = vartical height
+'''
+base = float(input("Enter Base : "))
+height = float(input("Enter Height : "))
+area = 0.5 * base * height
+print("Triangle Area = ",area)
+
+
+
+#circle
+'''
+radius = 3.1416 r^2
+'''
+radius = float(input("Enter Radius : "))
+area = 3.1416 * radius * radius
+print("Circle Area : ",area)
