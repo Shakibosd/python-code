ef46162f0a1c625ab0367f5f626beade5c3e5f97
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-04-13 01:26:11 +0600

    finish

---
+++ Math related Library functions.py
@@ -0,0 +1,19 @@
+# Python Bangla Tutorials 10 : Math related Library functions
+
+from math import *
+
+print("The Maximum Value Is = ",max(50,100,350,780,279,644))
+print("The Minimum Value Is = ",min(50,100,350,780,279,644))
+print("The Abs Value Is = ",abs(-4))
+print("The Pow Value Is = ",pow(5,6))
+print("The Sqrt Value Is = ",sqrt(25))
+print("The Round Value Is = ",round(3.8))
+print("The Floor Value Is = ",floor(3.7))
+print("The Ceil Value Is = ",ceil(3.7))
+print("The Tan Value Is = ",tan(20))
+print("The Tanh Value Is = ",tanh(50))
+print("The sin Value Is = ",sin(10))
+print("The perm Value Is = ",perm(10))
+print("The log10 Value Is = ",log10(5))
+print("The log1p Value Is = ",log1p(20))
+print("The log2 Value Is = ",log2(30))
